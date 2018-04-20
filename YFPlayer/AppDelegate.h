681ff2c3ff481bdc79287f1d46caf6ecfdedc6d8
bflyt <1057921792@qq.com>
2018-04-20 14:52:02 +0800

    第一次提交

---
+++ YFPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFPlayer
+//
+//  Created by jyzx101 on 2018/4/20.
+//  Copyright © 2018年 bflyT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YFPlayer/YFPlayer/YFPlayerControlProtocol.h
@@ -0,0 +1,14 @@
+//
+//  YFPlayerControlProtocol.h
+//  YFPlayer
+//
+//  Created by jyzx101 on 2018/4/20.
+//  Copyright © 2018年 bflyT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol YFPlayerControlProtocol <NSObject>
+
+
+@end
