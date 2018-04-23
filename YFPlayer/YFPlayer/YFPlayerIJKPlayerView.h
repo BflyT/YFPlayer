f14a9d2ff470ec0da4e636867e72552d52ad3005
bflyt <1057921792@qq.com>
2018-04-23 08:59:10 +0800

    更新read me

---
+++ YFPlayer/YFPlayer/YFPlayerIJKPlayerView.h
@@ -0,0 +1,14 @@
+//
+//  YFPlayerIJKPlayerView.h
+//  YFPlayer
+//
+//  Created by jyzx101 on 2018/4/20.
+//  Copyright © 2018年 bflyT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "YFPlayerControlProtocol.h"
+
+@interface YFPlayerIJKPlayerView : UIView <YFPlayerControlProtocol>
+
+@end
---
+++ YFPlayer/YFPlayer/YFPlayerIJKPlayerView.m
@@ -0,0 +1,21 @@
+//
+//  YFPlayerIJKPlayerView.m
+//  YFPlayer
+//
+//  Created by jyzx101 on 2018/4/20.
+//  Copyright © 2018年 bflyT. All rights reserved.
+//
+
+#import "YFPlayerIJKPlayerView.h"
+
+@implementation YFPlayerIJKPlayerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
